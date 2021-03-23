ae23329d413223fa88730e0f096cbbcd720e8f99
Premchand <premchandpurushothaman.jayachandran@sjsu.edu>
2021-03-22 21:38:04 -0700

    output file in the reqired format

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,7 +1,10 @@
 import pandas as pd
-
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
         
@@ -9,28 +12,38 @@ class DiabetesClassifier:
     def __init__(self) -> None:
         col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
         self.pima = pd.read_csv('diabetes.csv', header=0, names=col_names, usecols=col_names)
-        print(self.pima.head())
+        # print(self.pima.head()) to be used in case of issues
         self.X_test = None
         self.y_test = None
         
 
     def define_feature(self):
-        feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
+        feature_cols = ['pregnant','glucose', 'bp', 'bmi']
         X = self.pima[feature_cols]
         y = self.pima.label
         return X, y
     
-    def train(self):
+    def logistic_regression(self):
+        logreg = LogisticRegression()
+        return self.train_and_predict(logreg)
+
+    def sgd_classifier(self):
+        sgd_clf = SGDClassifier(max_iter=1000,tol=1e-3,random_state=0)
+        return self.train_and_predict(sgd_clf)
+
+    def decision_tree_classifier(self):
+        dt_clf = DecisionTreeClassifier(random_state = 0, max_depth = 4)
+        return self.train_and_predict(dt_clf)
+
+    def kneighbors_classifier(self):
+        kn_clf = KNeighborsClassifier(n_neighbors = 8)
+        return self.train_and_predict(kn_clf)
+
+    def train_and_predict(self,model):
         # split X and y into training and testing sets
         X, y = self.define_feature()
         X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, random_state=0)
-        # train a logistic regression model on the training set
-        logreg = LogisticRegression()
-        logreg.fit(X_train, y_train)
-        return logreg
-    
-    def predict(self):
-        model = self.train()
+        model.fit(X_train, y_train)
         y_pred_class = model.predict(self.X_test)
         return y_pred_class
 
@@ -51,10 +64,39 @@ class DiabetesClassifier:
     
 if __name__ == "__main__":
     classifer = DiabetesClassifier()
-    result = classifer.predict()
-    print(f"Predicition={result}")
-    score = classifer.calculate_accuracy(result)
-    print(f"score={score}")
-    con_matrix = classifer.confusion_matrix(result)
-    print(f"confusion_matrix=${con_matrix}")
+    lr_result = classifer.logistic_regression()
+    # print(f"Predicition={lr_result}") to be used in case of issues
+    lr_score = classifer.calculate_accuracy(lr_result)
+    # print(f"score={lr_score}")
+    lr_con_matrix = classifer.confusion_matrix(lr_result)
+    # print(f"confusion_matrix=${lr_con_matrix}") to be used in case of issues
     
+    sgd_result = classifer.sgd_classifier()
+    # print(f"Predicition={sgd_result}") to be used in case of issues
+    sgd_score = classifer.calculate_accuracy(sgd_result)
+    # print(f"score={sgd_score}")
+    sgd_con_matrix = classifer.confusion_matrix(sgd_result)
+    # print(f"confusion_matrix=${sgd_con_matrix}") to be used in case of issues
+
+    dt_result = classifer.decision_tree_classifier()
+    # print(f"Predicition={dt_result}") to be used in case of issues
+    dt_score = classifer.calculate_accuracy(dt_result)
+    # print(f"score={dt_score}") to be used in case of issues
+    dt_con_matrix = classifer.confusion_matrix(dt_result)
+    # print(f"confusion_matrix=${dt_con_matrix}") to be used in case of issues
+    
+    kn_result = classifer.kneighbors_classifier()
+    # print(f"Predicition={kn_result}") to be used in case of issues
+    kn_score = classifer.calculate_accuracy(kn_result)
+    # print(f"score={kn_score}") to be used in case of issues
+    kn_con_matrix = classifer.confusion_matrix(kn_result)
+    # print(f"confusion_matrix=${kn_con_matrix}") to be used in case of issues
+
+    output = pd.DataFrame(np.array([
+        ['logistic_regression' , lr_score , lr_con_matrix , 'My Logistic Regression solution and the labels are pregnant, glucose, bp, bmi' ],
+        ['SGDClssifier', sgd_score, sgd_con_matrix, 'solution with SGDC and the labels are pregnant, glucose, bp, bmi'],
+        ['DecisionTreeClassier',dt_score, dt_con_matrix, 'solution with DTC and the labels are pregnant, glucose, bp, bmi'],
+        ['KNeighborClassofier', kn_score, kn_con_matrix, 'solution with KNC and the labels are pregnant, glucose, bp, bmi']
+        ]), columns = ['Experiment','Accuracy','Confusion Matrix','Comment'])
+
+    print(output.to_markdown())
--- lab3/output.md
+++ lab3/output.md
@@ -1,6 +1,7 @@
-| Experiement | Accuracy | Confusion Matrix | Comment |
-|-------------|----------|------------------|---------|
-| Baseline    | 0.6770833333333334 | [[114  16] [ 46  16]] |  |
-| Solution 1   | xxxxxxx  | xxxxxx |  xxxxx |
-| Solution 2   | xxxxxxx  | xxxxxx |  xxxxx |
-| Solution 3   | xxxxxxx  | xxxxxx |  xxxxx |
+| Experiement               | Accuracy | Confusion Matrix     | Comment                                                                 |
+|---------------------------|----------|----------------------|-------------------------------------------------------------------------|
+| Baseline                  | 0.677083 | [[114  16] [46  16]] | Base solution                                                           |
+| logistic_regression       | 0.8215   | [[119  11] [25  37]] | solution with LGR and the labels are 'pregnant', 'glicose', 'bp', 'bmi' |
+| SGDClassifier             | 0.744792 | [[108  22] [27  35]] | solution with SGD and the labels are 'pregnant', 'glicose', 'bp', 'bmi' |
+| DecisionTreeClassifier    | 0.770833 | [[119  11] [36  26]] | solution with DTC and the labels are 'pregnant', 'glicose', 'bp', 'bmi' |
+| KNeighborClassifier       | 0.770833 | [[118  12] [32  30]] | solution with KNC and the labels are 'pregnant', 'glicose', 'bp', 'bmi' |
